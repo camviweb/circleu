19051789d5447619da39466b569329dcf5176ad6
camviweb <camvicreations@gmail.com>
2025-02-13 18:03:48 +0100

    base

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'blog')]
+    public function index(Request $request): Response
+    {
+        return $this->render('blog/index.html.twig');
+    }
+}
