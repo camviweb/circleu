8694d0299946951d723936fc56a424fede70db08
Jonathan Levy <jonathan.levy2@etu.univ-paris1.fr>
2025-02-17 23:52:53 +0100

    page accueil OK

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class BlogController extends AbstractController
+final class EventsController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
+    #[Route('/events', name: 'app_events')]
     public function index(): Response
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+        return $this->render('events/index.html.twig', [
+            'controller_name' => 'EventsController',
         ]);
     }
 }
