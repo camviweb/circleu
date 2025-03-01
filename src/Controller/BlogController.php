7e057c96188d8945cca04b593b2abed0cc7206a2
camviweb <camvicreations@gmail.com>
2025-03-01 12:42:53 +0100

    page twig blog v1

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -11,8 +11,33 @@ final class BlogController extends AbstractController
     #[Route('/blog', name: 'app_blog')]
     public function index(): Response
     {
+        $posts = [
+            [
+                'user' => [
+                    'name' => 'Emily',
+                ],
+                'date' => new \DateTime('2025-02-20 15:30'),
+                'type' => 'alumni',
+                'content' => "J'ai participé au CU. MUN !",
+                'image' => 'images/post1.png',
+                'comments' => [
+                    ['user' => ['name' => 'Bob'], 'content' => "Wow! Comment s'inscrire ?"],
+                    ['user' => ['name' => 'Alice'], 'content' => 'Félicitations !']
+                ]
+            ],
+            [
+                'user' => [
+                    'name' => 'Damian',
+                ],
+                'date' => new \DateTime('2025-02-18 13:37'),
+                'type' => 'question',
+                'content' => 'Comment faire ... ?',
+                'image' => null,
+                'comments' => []
+            ]
+        ];
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'posts' => $posts,
         ]);
     }
 }
